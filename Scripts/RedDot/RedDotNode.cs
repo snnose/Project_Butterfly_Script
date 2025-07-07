c16aa926cb4be02410a3c6174001f2935edb96a7
Seoko <teary1@naver.com>
2025-07-08 06:26:31 +0900

    작업 스크립트 추가

---
+++ Scripts/RedDot/RedDotNode.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RedDot
+{
+    public class RedDotNode : MonoBehaviour
+    {
+        public List<string> Subscribe;
+
+        public string itemType;
+
+        public GameObject redDot;  
+
+        private void Awake()
+        {
+            RedDotSystem.OnRedDotChange += OnRedDotChange;
+        }
+
+        private void OnEnable()
+        {
+            RefreshRedDot();
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        bool IsContainItem(string eRedDotItem)
+        {
+            return Subscribe.Contains(eRedDotItem);
+        }
+
+        private int GetTotalRedDotCount()
+        {
+            int totalCount = 0;
+
+            for (int i = 0; i < Subscribe.Count; i++)
+            {
+                totalCount += RedDotSystem.GetRedDotCount(Subscribe[i]);
+            }
+
+            //Debug.Log("itemType : " + itemType + " / totalCount : " + totalCount);
+
+            return totalCount;
+        }
+
+        public void RefreshRedDot()
+        {
+            int totalRedDotCount = GetTotalRedDotCount();
+
+            // itemType이 null이 아니면 진입 (itemType이 null이면 잎 노드)
+            if (this.itemType != null)
+            {
+                // totalRedDotCount 개수로 현재 노드를 활성화 여부를 판단 (Count가 0이면 변화 없음)
+                RedDotSystem.SetRedDot(this.itemType, totalRedDotCount);
+            }
+
+            //Debug.Log("RefreshRedDot()");
+
+            if (totalRedDotCount > 0)
+            {
+                redDot.SetActive(true);
+            }
+            else
+            {
+                redDot.SetActive(false);
+            }
+        }
+
+        private void OnRedDotChange(string eRedDotItem)
+        {
+            if (IsContainItem(eRedDotItem))
+            {
+                RefreshRedDot();
+            }
+        }
+    }
+}
---
+++ Scripts/RedDot/RedDotSystem.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RedDot
+{
+    // 25.01.16) 일단 Collection으로 테스트 한 결과
+    // 1. 결론적으로 우선 드롭을 새로 획득할 때 RedDot은 잘 출력 됨
+    // 2. 문제는 획득 후 로비로 돌아왔을 때 컬렉션 버튼에 RedDot 출력이 바로 안됨
+    // 3. 근데 컬렉션 버튼을 누르면 바로 RedDot이 출력됨
+    // 4. 새 드롭을 얻기 전 컬렉션을 한 번이라도 진입하면 정상적으로 RedDot 출력됨
+
+    public static class RedDotSystem
+    {
+        public static Action<string> OnRedDotChange;
+
+        static Dictionary<string, int> eRedDotCounter = new Dictionary<string, int>();
+
+        public static void Initialize(string eRedDotItem)
+        {
+            if (eRedDotCounter.ContainsKey(eRedDotItem))
+            {
+                eRedDotCounter.Remove(eRedDotItem);
+            }
+
+            OnRedDotChange?.Invoke(eRedDotItem);
+        }
+
+        public static void AddRedDot(string eRedDotItem, int count)
+        {
+            if (eRedDotCounter.ContainsKey(eRedDotItem))
+            {
+                eRedDotCounter[eRedDotItem] += count;
+            }
+            else
+            {
+                eRedDotCounter[eRedDotItem] = count;
+            }
+
+            // PlayerPrefs에 레드닷 정보 저장
+            // FIXME : key값으로 dropId를 바로 집어 넣는 게 좋은 방법은 아닌 거 같다. 해결 방안 생각해야함
+            PlayerPrefs.SetInt(eRedDotItem, count);
+
+            OnRedDotChange?.Invoke(eRedDotItem);
+        }
+
+        public static void SetRedDot(string eRedDotItem, int count)
+        {            
+            eRedDotCounter[eRedDotItem] = count;
+            
+            // PlayerPrefs에 레드닷 정보 저장
+            // FIXME : key값으로 dropId를 바로 집어 넣는 게 좋은 방법은 아닌 거 같다. 해결 방안 생각해야함
+            PlayerPrefs.SetInt(eRedDotItem, count);
+
+            OnRedDotChange?.Invoke(eRedDotItem);
+        }
+
+        public static void ClearRedDot(string eRedDotItem)
+        {            
+            eRedDotCounter[eRedDotItem] = 0;
+
+            // PlayerPrefs에 저장된 레드닷 정보 초기화
+            PlayerPrefs.SetInt(eRedDotItem, 0);
+
+            OnRedDotChange?.Invoke(eRedDotItem);
+        }
+
+        public static int GetRedDotCount(string eRedDotItem)
+        {
+            if (eRedDotCounter.ContainsKey(eRedDotItem))
+            {
+                return eRedDotCounter[eRedDotItem];
+            }
+
+            return 0;
+        }
+    }
+}
